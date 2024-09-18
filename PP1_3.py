a42d912506ead7726d000dc73e7771f762986423
Andy-Chow-ICD2O1-02 <andy.chow2@student.tdsb.on.ca>
2024-09-18 17:15:48 +0000

    finished assignment

--- PP1_3.py
+++ PP1_3.py
@@ -1,20 +1,29 @@
-
+'''
+Title: Newline character, mod, print without newline
+Author: Andy Chow
+Date Created: September 18th, 2024
+Date Last Modified: September 18th, 2024
+'''
 
 def q1():
+  print("* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n""\n* * * * *\n*       *\n*       *\n*       *\n* * * * *\n""\n* * * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *\n")
   #Write Assignment code here
   #Remember your code must be indented once
 
 def q2():
+  print("Hello ", end = "")
+  print("World\n")
   #Write Assignment code here
   
 def q3():
+  print('"Man"\n"Board"\n"Man Overboard"')
   #Write Assignment code here
   
-def q4():
-  #Write Assignment code here
+
+
 
 #Do not alter anything after this line
 q1()
 q2()
 q3()
-q4()
+
